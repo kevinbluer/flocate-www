d89e13de95419808fdd042e974bb57aae5225ce0
Kevin Bluer <kevin@bluer.com>
2014-12-25 17:54:58 +0800

    trip now works

--- client/app/viewTrip/viewTrip.controller.js
+++ client/app/viewTrip/viewTrip.controller.js
@@ -3,23 +3,68 @@
 angular.module('flocateApp')
   .controller('ViewtripCtrl', function($scope, $http, $stateParams) {
 
-  	$http.get('/api/trip/' + $stateParams.tripId).
-	success(function(data, status, headers, config) {
+  	$scope.mapOptions = {
+      center: new google.maps.LatLng(22.32532675380104, 114.169360706689),
+      zoom: 1,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
 
-		console.log(data);
+    var infowindow;
+    var checkins;
+    var currentLocation = 0;
 
-		$scope.name = data[0].Name;
-		$scope.description = data[0].Description;
+  	$scope.initMap = function() {
 
-		$scope.checkins = data[1];
+  		// load the pins from parse
+  		$http.get('/api/trip/' + $stateParams.tripId).
+		  success(function(data, status, headers, config) {
 
-	}).
-	error(function(data, status, headers, config) {
+		  	$scope.name = data[0].Name;
+			$scope.description = data[0].Description;
+			$scope.checkins = data[1];
 
-		console.log(data);
+			// console.log(data[1]);
 
-		// TODO handle the error scenarios
+			// return;
 
-	});
+		 //  	checkins = JSON.parse($scope.checkins);
+
+		  	angular.forEach($scope.checkins, function(value, key) {
+
+			    var marker = new google.maps.Marker({
+			        map : $scope.myMap,
+			        position : new google.maps.LatLng(value["Location"]["latitude"], value["Location"]["longitude"])
+			    });
+
+			    if (currentLocation == 0) {
+			    	$("#pinTitle").html(value["Doing"]);
+			    	$("#pinDesc").html(value["Note"]);
+			    	$scope.pinDate = value["RecordedAt"]["iso"];
+			    	$scope.checkinId = value.objectId;
+			    	$scope.myMap.setCenter(new google.maps.LatLng(value["Location"]["latitude"], value["Location"]["longitude"]));
+			    }
+
+			    currentLocation += 1;
+
+		    	google.maps.event.addListener(marker, 'click', function() {
+				    
+				    $("#pinTitle").html(value["Doing"]);
+				    $("#pinDesc").html(value["Note"]);
+				    $scope.checkinId = value.objectId;
+				    $scope.pinDate = value["RecordedAt"]["iso"];
+				});
+
+			    $scope.newMarker = marker;
+
+			});
+
+
+		  }).
+		  error(function(data, status, headers, config) {
+		  	console.log(data);
+		    // called asynchronously if an error occurs
+		    // or server returns response with an error status.
+		  });
+	};
 
   });
