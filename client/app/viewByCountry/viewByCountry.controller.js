fc6d1c6381b057b2432c529fa396a0970ca4ebfe
Kevin Bluer <kevin@bluer.com>
2015-01-01 18:17:12 +0800

    some additional stubbing for the country view

--- client/app/viewByCountry/viewByCountry.controller.js
+++ client/app/viewByCountry/viewByCountry.controller.js
@@ -2,5 +2,18 @@
 
 angular.module('flocateApp')
   .controller('ViewByCountryCtrl', function ($scope) {
-    $scope.message = 'Hello';
+
+  	// Note that the LatLng / Zoom should come from the country table
+
+  	// TODO - make call to get the country
+  	// TODO - cap the zoom level on view?
+  	// TODO OR - consider dynamically changing the country name / etc (on the model of a master map)
+  	// TODO - include the trips that span China too (e.g. backbacking through China with Stokesy)
+
+    $scope.mapOptions = {
+      center: new google.maps.LatLng(34.95799531086792, 103.447265625),
+      zoom: 4,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
   });
