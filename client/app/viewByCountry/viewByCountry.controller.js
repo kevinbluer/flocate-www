02b8cb4e250ca975d5dae908f18e525bad75318d
Kevin Bluer <kevin@bluer.com>
2015-02-03 10:31:25 +0800

    tweakin

--- client/app/viewByCountry/viewByCountry.controller.js
+++ client/app/viewByCountry/viewByCountry.controller.js
@@ -6,24 +6,22 @@ angular.module('flocateApp')
   	// TODO - cap the zoom level on view?
   	// TODO - include the trips that span China too (e.g. backbacking through China with Stokesy)
 
+  	// do lookup based on country code
   	$scope.mapOptions = {
-      center: new google.maps.LatLng(0.1, 0.1),
-      zoom: 3,
+      center: new google.maps.LatLng(25, 0.1),
+      zoom: 2,
       mapTypeId: google.maps.MapTypeId.ROADMAP
     };
-
-  	// do lookup based on country code
 
   	$http.post('/api/locations/getCountryDetails', {"CountryCode3": $stateParams.countryCode}).
   		success(function(data, status, headers, config) {
 
   			$scope.countryName = data[0].CountryName;
 
-  			$scope.mapOptions = {
-		      center: new google.maps.LatLng(0.3, 0.3),
-		      zoom: 15,
-		      mapTypeId: google.maps.MapTypeId.ROADMAP
-		    };
+  			console.log(data[0]);
+
+			$scope.myMap.setCenter(new google.maps.LatLng(data[0].CountryCenterPoint.latitude, data[0].CountryCenterPoint.longitude));
+			$scope.myMap.setZoom(data[0].ZoomLevel);
 
 			$http.get('/api/checkin/checkinsByCountry', {}).
 			success(function(data, status, headers, config) {
