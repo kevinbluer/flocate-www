890b3f353ab23c237517c06ff08919b8305a563c
Kevin Bluer <kevin@bluer.com>
2015-03-01 20:03:47 +0800

    preliminary settings stuff

--- client/app/settings/settings.controller.js
+++ client/app/settings/settings.controller.js
@@ -1,6 +1,10 @@
 'use strict';
 
 angular.module('flocateApp')
-  .controller('SettingsCtrl', function ($scope) {
-    $scope.message = 'Hello';
+  .controller('SettingsCtrl', function ($scope, Auth) {
+  	var user = Auth.getCurrentUser();
+    $scope.firstname = user.firstName;
+    $scope.lastname = user.lastName;
+    $scope.email = user.email;
+    $scope.location = user.location;
   });
--- server/api/user/user.controller.js
+++ server/api/user/user.controller.js
@@ -1,16 +1,14 @@
 'use strict';
 
 var _ = require('lodash');
+var Parse = require('parse').Parse;
+
 
 exports.index = function(req, res) {
   res.json([]);
 };
 
 exports.me = function(req, res, next) {
-
-  // TODO - Put in error handling
-
-  var Parse = require('parse').Parse;
   Parse.initialize("VVbJ2YjOdDJrY7sZw8fF4R1v2Wolgf3toi4o5SW0", "4zEOZdifrLJxr2exozJMGsE8SB7zmnienaMMsjTF");
   res.json(Parse.User.current());
 };
