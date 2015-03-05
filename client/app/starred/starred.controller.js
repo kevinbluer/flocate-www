28ab81cc88a69448db8e03ffcab4717ed46dd9dc
Kevin Bluer <kevin@bluer.com>
2015-03-05 22:57:59 +0800

    preliminary stubbing of starred places page

--- client/app/starred/starred.controller.js
+++ client/app/starred/starred.controller.js
@@ -1,6 +1,19 @@
 'use strict';
 
 angular.module('flocateApp')
-  .controller('StarredCtrl', function ($scope) {
-    $scope.message = 'Hello';
+  .controller('StarredCtrl', function ($scope, $http) {
+
+	$http.get('/api/checkin/getStarredCheckins', {}).
+      success(function(data, status, headers, config) {
+
+        $scope.moment = moment;
+        $scope.starred = data;
+
+      }).
+      error(function(data, status, headers, config) {
+
+        // TODO handle the error scenarios
+
+     });
+
   });
