744f4ed608a7ef0114fe66519e8c4b86b02f6176
Kevin Bluer <kevin@bluer.com>
2015-03-27 01:07:00 +0800

    properly storing country last visited

--- cloud-code/cloud/main.js
+++ cloud-code/cloud/main.js
@@ -14,19 +14,42 @@ Parse.Cloud.afterSave("_User", function(request) {
 
 Parse.Cloud.afterSave("Checkin", function(request) {
 
-	var user = Parse.User.current();
-	user.set("LastCountry", {CountryCode2: request.object.get("CountryCode2")});
+	// TODO do the lookup to the Country class and store both country code lengths
 
-	user.save(null, {
-	  success: function(user) {
-	    // Execute any logic that should take place after the object is saved.
-	    console.log("updated user");
-	    
-	  },
-	  error: function(gameScore, error) {
-	    // Execute any logic that should take place if the save fails.
-	    // error is a Parse.Error with an error code and message.
-	    console.log('Failed to create new object, with error code: ' + error.message);
-	  }
-	});
+	var countryCode = request.object.get("CountryCode2");
+
+	if (countryCode.length === 2) {
+
+		var query = new Parse.Query("Countries");
+		query.equalTo("CountryCode2", countryCode);
+		query.first({
+			success: function(country) {
+			  
+				var lastCountry = {CountryCode2: country.get("CountryCode2"), CountryCode3: country.get("CountryCode3") };
+
+				var user = Parse.User.current();
+				user.set("LastCountry", lastCountry);
+
+				user.save(null, {
+				  success: function(user) {
+				    // Execute any logic that should take place after the object is saved.
+				    console.log("updated user");
+				    
+				  },
+				  error: function(gameScore, error) {
+				    // Execute any logic that should take place if the save fails.
+				    // error is a Parse.Error with an error code and message.
+				    console.log('Failed to create new object, with error code: ' + error.message);
+				  }
+				});
+
+		},
+			error: function(error) {
+			  console.error("Got an error " + error.code + " : " + error.message);
+			}
+		});
+
+	}
+
+	
 });
